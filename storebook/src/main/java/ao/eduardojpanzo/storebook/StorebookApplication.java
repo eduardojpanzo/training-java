0ed2cf5fa848fb132c0c28abc464e1631fd1e842
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2024-10-18 08:57:34 +0100

    initialize book store project

---
+++ storebook/src/main/java/ao/eduardojpanzo/storebook/StorebookApplication.java
@@ -0,0 +1,13 @@
+package ao.eduardojpanzo.storebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorebookApplication.class, args);
+	}
+
+}
