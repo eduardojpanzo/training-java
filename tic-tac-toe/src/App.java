b857eece72968ab29e166c8027c3edddc01343d6
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2026-01-14 20:17:08 +0100

    feat: add tic-tac-toe project

---
+++ tic-tac-toe/src/App.java
@@ -0,0 +1,7 @@
+import javax.swing.SwingUtilities;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        SwingUtilities.invokeLater(TicTacToe::new);
+    }
+}
